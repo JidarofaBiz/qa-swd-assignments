773f5eb91f9292bd12d9a50598d1b150f0341b63
JidarofaBiz <jimmy.rodriguezf@bizagi.com>
2018-06-18 21:32:21 -0500

    First Push to Primes

---
+++ Primes/src/CircularPrimes.java
@@ -0,0 +1,36 @@
+
+public class CircularPrimes {
+
+	public boolean toPrime(int number) {
+			
+			int cont = 0; 	
+			for(int j = 1; j <= number; j++) {
+				if (number%j == 0) {
+					cont++;			
+				}
+			}
+			if (cont <=2) {
+				return true;
+			}						
+			return false;
+	}
+	
+	public boolean toCircularPrime(int number) {
+		
+		Integer wrapperNumber = new Integer(number);
+		int cont = 0; 
+		String a = wrapperNumber.toString(); 
+		
+		for (int i = 1; i <= wrapperNumber.toString().length(); i++) {				
+			if (toPrime(Integer.parseInt(a))) {
+				cont++;
+			}
+		    a = new StringBuffer(a).append(a.charAt(0)).toString();		
+			a = a.substring(1);		
+		}			
+		if (cont == wrapperNumber.toString().length()) {
+			return true;
+		}			
+		return false;
+	}
+}
--- Primes/src/Primes.java
+++ Primes/src/Primes.java
@@ -1,4 +1,16 @@
 
 public class Primes {
-
+	
+	public static void main (String[] args) {
+		
+		CircularPrimes primes = new CircularPrimes();
+		
+		for(int i = 2; i <= 1000000; i++) {		
+			if (primes.toPrime(i)) {				
+				if (primes.toCircularPrime(i)) {				
+					System.out.println(i);
+				}
+			}
+		}				
+	}
 }
